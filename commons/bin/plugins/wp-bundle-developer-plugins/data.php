1d7bbe6b826a60e42fec0621e5e4c625133b0d08
dev-cbos-ca <dev@cbos.ca>
2018-05-05 12:57:24 -0400

    Update.

--- commons/bin/plugins/wp-bundle-developer-plugins/data.php
+++ commons/bin/plugins/wp-bundle-developer-plugins/data.php
@@ -2,42 +2,9 @@
 
 defined( 'ABSPATH' ) || exit;
 
-function get_wp_bundle_developer_plugins_data(){
-	$items = array( 
-		array ( 
-			'display' => 1,
-			'category' => 'optimization', 
-			'title' => 'P3 (Plugin Performance Profiler)', 
-			'name' => 'p3-profiler', 
-			'uri' => 'https://wordpress.org/plugins/p3-profiler/', 
-			'notes' => '',
-		),
-		array( 
-			'display' => 1,
-			'category' => 'optimization', 
-			'title' => 'Image Cleanup', 
-			'name' => 'image-cleanup', 
-			'uri' => 'https://wordpress.org/plugins/image-cleanup/', 
-			'details' => 'image+cleanup',
-			'notes' => 'DB and file backup recommended.', 
-			),
-		array( 
-			'display' => 1,
-			'category' => 'optimization', 
-			'title' => 'WP-Sweep', 
-			'name' => 'wp-sweep', 
-			'uri' => 'https://wordpress.org/plugins/wp-sweep/', 
-			'notes' => 'DB backup recommended.', 
-			),
-		array( 
-			'display' => 1,
-			'category' => 'optimization', 
-			'title' => 'WP Smush', 
-			'name' => 'wp-smushit', 
-			'uri' => 'https://wordpress.org/plugins/wp-smushit/', 
-			'notes' => 'DB and file backup suggested.',
-			),		
-		);
-		return $items;
+function get_wp_bundle_developer_plugins_switch(){
+	$items = [ 
+		'keep' => 0,
+	];
+	return $items;
 }
-
--- commons/bin/plugins/wp-bundle-developer-plugins/logic.php
+++ commons/bin/plugins/wp-bundle-developer-plugins/logic.php
@@ -7,21 +7,26 @@ function run_wp_bundle_developer_plugins(){
 	if ( 1 && current_user_can( 'manage_options' ) ) {
 		if ( function_exists( 'get_plugins' ) ) {
 			$plugins = get_plugins();
-			$file = ( __DIR__ . '/data/plugins.php' );
+			$switch = get_wp_bundle_developer_plugins_switch();
+			$file = ( __DIR__ . '/data/printed.php' );
 			$str = '<?php' . PHP_EOL;
 			$str .= PHP_EOL;
-			$str .= '$items = [ ' . PHP_EOL;
+			$str .= 'defined( \'ABSPATH\' ) || exit;' . PHP_EOL;
+			$str .= PHP_EOL;
+			$str .= 'function get_wp_bundle_developer_plugins_data() {' . PHP_EOL;
+			$str .= PHP_EOL;
+			$str .= "\t" . '$items = [ ' . PHP_EOL;
 			foreach ( $plugins as $k => $plugin ){
-				$str .= "\t" . '[ ' . $k . ', ';
-				if ( is_plugin_active( $k ) ){
-					$str .= 'active => 1, ';
+				$str .= "\t\t" . '[ \'plugin\' => \'' . $k . '\', ';
+				if ( is_plugin_active( $k ) && $switch['keep'] ){
+					$str .= '\'active\' => 1, ';
 				} else {
-					$str .= 'active => 0, ';
+					$str .= '\'active\' => 0, ';
 				}
 				$str .= '],' . PHP_EOL;
 			}
-			$str .= ']; '. PHP_EOL;
-			
+			$str .= "\t" . ']; '. PHP_EOL;
+			$str .= '};'. PHP_EOL;
 			$result = file_put_contents( $file, $str );
 		}
 		else {
@@ -29,3 +34,26 @@ function run_wp_bundle_developer_plugins(){
 		}
 	}
 }
+
+function wp_bundle_developer_plugins_activate( $plugins ){
+	require_once( dirname(__FILE__) . '/data/plugins.php' );
+	$plugins = get_plugins_data();
+	if ( ! empty ( $plugins ) )  {
+		foreach ( $plugins as $plugin ) {
+			$file = WP_PLUGIN_DIR  . '/' .  $plugin['folder'] . '/' . $plugin['file'];
+			if ( file_exists( $file ) ) {
+				if ( $plugin['activate'] ) {
+					if ( ( $plugin['local'] && is_localhost() ) || ( $plugin['online'] && ! is_localhost() ) ) {
+						activate_plugin( $file );
+						if ( isset( $plugin['configure'] ) && $plugin['configure'] && isset( $plugin['settings'] ) ) {
+							foreach ( $plugin['settings'] as $setting ) {
+								update_option( $setting['option_name'], $setting['option_value'] );
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
