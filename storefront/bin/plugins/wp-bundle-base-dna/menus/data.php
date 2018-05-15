2c0b66ee56b91527be0b385505b799a907ae3f46
dev-cbos-ca <dev@cbos.ca>
2018-05-15 10:21:48 -0400

    Update.

--- storefront/bin/plugins/wp-bundle-base-dna/menus/data.php
+++ storefront/bin/plugins/wp-bundle-base-dna/menus/data.php
@@ -8,17 +8,25 @@ function get_menus_data() {
 			'name' => 'Header Menu', 'slug' => 'header-menu', 'build' => 1,
 			'items' => array (
 				array ( 'title' => 'Home', 'slug' => '', 'build' => 1 ), //slug should be empty
-				array ( 'title' => 'Blog', 'slug' => 'blog', 'build' => 1 ),
-				array ( 'title' => 'About', 'slug' => 'about', 'build' => 1 ),
+				array ( 'title' => 'Catalog', 'slug' => 'catalog', 'build' => 1 ),
+				array ( 'title' => 'Shipping', 'slug' => 'shipping', 'build' => 1 ),
 				array ( 'title' => 'Contact', 'slug' => 'contact', 'build' => 1 ),
 				),
 		),
 		array (
-			'name' => 'Footer Menu', 'slug' => 'footer-menu', 'build' => 1,
+			'name' => 'Cart', 'slug' => 'cart', 'build' => 0,
 			'items' => array (
-				array ( 'title' => 'Terms', 'slug' => 'terms', 'build' => 1 ),
-				array ( 'title' => 'Privacy', 'slug' => 'privacy', 'build' => 1 ),
-				array ( 'title' => 'Contact', 'slug' => 'contact', 'build' => 1 ),
+				array ( 'title' => 'My Account', 'slug' => 'my-account', 'build' => 1 ),
+				array ( 'title' => 'Cart', 'slug' => 'cart', 'build' => 1 ),
+				),
+			),
+		array (
+			'name' => 'Footer Menu', 'slug' => 'footer-menu', 'build' => 0,
+			'items' => array (
+				array ( 'title' => 'About', 'slug' => 'about', 'build' => 1 ),
+				array ( 'title' => 'Terms and Conditions', 'slug' => 'terms', 'build' => 1 ),
+				array ( 'title' => 'Privacy Policy', 'slug' => 'privacy', 'build' => 1 ),
+				array ( 'title' => 'Site Map', 'slug' => 'site-map', 'build' => 1 ),
 				),
 			),
 	);
